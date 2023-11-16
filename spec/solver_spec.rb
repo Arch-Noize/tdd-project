00695031e1e25df0e8ef006196b2bc71f0a9fc14
Arch-Noize <santiagonoize@gmail.com>
2023-11-16 11:44:34 -0400

    Add more factorial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,6 +7,14 @@ describe Solver do
     it 'returns 1 if 0!' do
       expect(Solver.factorial(0)).to eq(1)
     end
+
+    it '7! should return 5040' do
+        expect(Solver.factorial(7)).to eq(5040)
+    end
+
+    it 'should raise error if given negative number' do
+        expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
   end
 
   describe 'Reverse words' do
@@ -19,7 +27,7 @@ describe Solver do
     it 'returns "fizz" when N is divisible by 3' do
       expect(Solver.fizzbuzz(9)).to eq('fizz')
     end
-  
+
     it 'returns "buzz" when N is divisible by 5' do
       expect(Solver.fizzbuzz(10)).to eq('buzz')
     end
