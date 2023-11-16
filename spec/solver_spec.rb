3aaa03f0dbf18fda6b97eb5291922bd78fbfb3f9
Arch-Noize <santiagonoize@gmail.com>
2023-11-16 08:34:10 -0400

    Add empty tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,15 @@
+require './solver'
+
+describe Solver do
+    describe 'Solve factorials' do
+        
+    end
+
+    describe 'Reverse words' do
+
+    end
+
+    describe 'Fizzbuzz?' do
+
+    end
+end
