948752f2b88ba156fdfe93e40c8feafc0dd38fe1
Arch-Noize <santiagonoize@gmail.com>
2023-11-16 08:38:45 -0400

    Add test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,7 +2,13 @@ require './solver'
 
 describe Solver do
     describe 'Solve factorials' do
-        
+        before :each do
+            solver = Solver.new
+        end
+
+        it 'returns 1 if 0!' do
+            expect(solver.factorial(0)).to eq(1)
+        end
     end
 
     describe 'Reverse words' do
